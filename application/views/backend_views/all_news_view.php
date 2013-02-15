8aae6c53442661ced4713b4759c92c0a3c8c60ee
ynsekiz <ynsekiz@gmail.com>
2013-02-15 20:01:32 +0200

    tüm haberler formu yenilendi
    detaylı açıklama daha sonra verilecek

--- application/views/backend_views/all_news_view.php
+++ application/views/backend_views/all_news_view.php
@@ -18,11 +18,11 @@
 							
 							<thead>
 								<tr>
-									<th style="margin-left:270px;">Düzenle/Sil</th>
+									<th style="margin-left:270px;">Düzenle</th>
+								    <th>Haberi Sil</th>
 								    <th>Haber Tarihi</th>
-								    <th>Haber İçeriği</th>
 								    <th></th>
-		
+								    <th>Haber İçeriği</th>
 								</tr>
 								
 							</thead>
@@ -30,35 +30,69 @@
 						 
 							<tbody>
 								<tr>
-									<td><input type="radio" name="radio_field" value="1"/><a href="" title="Edit"><img src="{base}assets/backend_assets/images/icons/pencil.png" alt="Edit" /></a>
-										<input type="radio" name="radio_field" value="1"/><a href="" title="Delete"><img src="{base}assets/backend_assets/images/icons/cross.png" alt="Delete" /></a></td>
+									<td><input type="radio" name="radio_field" value="1"/><a href="" title="Edit"><img src="{base}assets/backend_assets/images/icons/pencil.png" alt="Edit" /></a></td>
+									<td><input type="radio" name="radio_field" value="1"/><a href="" title="Delete"><img src="{base}assets/backend_assets/images/icons/cross.png" alt="Delete" /></a></td>
 									<td>Lorem ipsum dolor</td>
-									<td>İlk işimizi işmizi yaptıkİlk işimizi işmizi yaptıkİlk işimizi işmizi yaptık</td>
 									<td></td>
-									
+									<td>İlk işimizi işmizi yaptıkİlk işimizi işmizi yaptıkİlk işimizi işmizi yaptık</td>
 								</tr>
 								
 								<tr>
-									<td><input type="radio" name="radio_field" value="1"/><a href="" title="Edit"><img src="{base}assets/backend_assets/images/icons/pencil.png" alt="Edit" /></a>
-										<input type="radio" name="radio_field" value="1"/><a href="" title="Delete"><img src="{base}assets/backend_assets/images/icons/cross.png" alt="Delete" /></a></td>
+									<td><input type="radio" name="radio_field" value="1"/><a href="" title="Edit"><img src="{base}assets/backend_assets/images/icons/pencil.png" alt="Edit" /></a></td>
+									<td><input type="radio" name="radio_field" value="1"/><a href="" title="Delete"><img src="{base}assets/backend_assets/images/icons/cross.png" alt="Delete" /></a></td>
 									<td>Lorem ipsum dolor</td>
-									<td>İlk işimizi işmizi yaptıkİlk işimizi işmizi yaptıkİlk işimizi işmizi yaptık</td>
 									<td></td>
-									
+									<td>İlk işimizi işmizi yaptıkİlk işimizi işmizi yaptıkİlk işimizi işmizi yaptık</td>
 								</tr>
 								
 								<tr>
-									<td><input type="radio" name="radio_field" value="1"/><a href="" title="Edit"><img src="{base}assets/backend_assets/images/icons/pencil.png" alt="Edit" /></a>
-										<input type="radio" name="radio_field" value="1"/><a href="" title="Delete"><img src="{base}assets/backend_assets/images/icons/cross.png" alt="Delete" /></a></td>
+									<td><input type="radio" name="radio_field" value="1"/><a href="" title="Edit"><img src="{base}assets/backend_assets/images/icons/pencil.png" alt="Edit" /></a></td>
+									<td><input type="radio" name="radio_field" value="1"/><a href="" title="Delete"><img src="{base}assets/backend_assets/images/icons/cross.png" alt="Delete" /></a></td>
 									<td>Lorem ipsum dolor</td>
-									<td>İlk işimizi işmizi yaptıkİlk işimizi işmizi yaptıkİlk işimizi işmizi yaptık</td>
 									<td></td>
-									
+									<td>İlk işimizi işmizi yaptıkİlk işimizi işmizi yaptıkİlk işimizi işmizi yaptık</td>
 								</tr>
-
-
 							</tbody>
 							
+							<tfoot>
+								<tr>
+									<td colspan="6">
+										<div class="bulk-actions align-left">
+											<a class="button" href="#">Seçili Eylemi Uygula</a>
+										</div>
+										
+										<div class="clear"></div>
+									</td>
+								</tr>
+							</tfoot>
+<!--
+							<tfoot>
+								<tr>
+									<td colspan="6">
+										<div class="bulk-actions align-left">
+											<select name="dropdown">
+												<option value="option1">Choose an action...</option>
+												<option value="option2">Edit</option>
+												<option value="option3">Delete</option>
+											</select>
+											<a class="button" href="#">Apply to selected</a>
+										</div>
+										
+										<div class="pagination">
+											<a href="#" title="First Page">&laquo; First</a><a href="#" title="Previous Page">&laquo; Previous</a>
+											<a href="#" class="number" title="1">1</a>
+											<a href="#" class="number" title="2">2</a>
+											<a href="#" class="number current" title="3">3</a>
+											<a href="#" class="number" title="4">4</a>
+											<a href="#" title="Next Page">Next &raquo;</a><a href="#" title="Last Page">Last &raquo;</a>
+										</div>
+										<div class="clear"></div>
+-->
+									</td>
+								</tr>
+							</tfoot>							
+
+
 						</table>
 
 					  </form>	
